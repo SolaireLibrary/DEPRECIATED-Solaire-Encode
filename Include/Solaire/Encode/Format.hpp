201bde70fa5a0d5ab19ce1b32c6da12de1758a59
SolaireLibrary <solairelibrary@mail.com>
2016-01-16 12:33:21 +0000

    Documentation for Format

--- Include/Solaire/Encode/Format.hpp
+++ Include/Solaire/Encode/Format.hpp
@@ -38,17 +38,57 @@
 
 namespace Solaire {
 
+    /*!
+        \brief Implements a storage format for encoding and decoding C++ objects.
+        \version 1.0.0
+    */
 	SOLAIRE_EXPORT_INTERFACE Format {
     public:
+        /*!
+            \brief Destroy the Format object.
+        */
         SOLAIRE_EXPORT_CALL ~Format(){}
+
+        /*!
+            \brief Decode data from the storage format into GenericValue format.
+            \param aStream The source of encoded data.
+            \return The decoded data.
+        */
         GenericValue SOLAIRE_EXPORT_CALL readValue(IStream&) const throw();
+
+        /*!
+            \brief Encode data into the storage format from GenericValue format.
+            \param aValue The data to encode.
+            \param aStream The place to store the encoded data.
+            \return True if the data was encoded successfully.
+        */
         bool SOLAIRE_EXPORT_CALL writeValue(const GenericValue&, OStream&) const throw();
 
+        /*!
+            \brief Decode a C++ object in place.
+            \details Passes the output of readValue into Encoder<T>::decode.
+            \tparam T The type of the object being decoded.
+            \param aAllocator The allocator to allocate the object, and any parseing data from.
+            \param aStream The source of encoded data.
+            \see readValue
+            \see Encoder::decode
+        */
         template<class T>
         SOLAIRE_FORCE_INLINE typename Encoder<T>::DecodeType read(Allocator& aAllocator, IStream& aStream) {
             return Encoder<T>::decode(aAllocator, readValue(aStream));
         }
 
+        /*!
+            \brief Encode a C++ object in place.
+            \details Passes the output of Encoder<T>::encode into writeValue.
+            \tparam T The type of the object being encoded.
+            \param aAllocator The allocator to allocate any parseing data from.
+            \param aValue The object being encoded.
+            \param aStream The place to store the encoded data.
+            \return True if the object was encoded successfully.
+            \see writeValue
+            \see Encoder::encode
+        */
         template<class T>
         SOLAIRE_FORCE_INLINE bool write(Allocator& aAllocator, const T& aValue, OStream& aStream) {
             return writeValue(Encoder<T>::encode(aAllocator, aValue), aStream);
